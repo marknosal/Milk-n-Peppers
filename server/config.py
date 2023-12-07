743813758bcd9e0830d8c70fc55c965f0988da3c
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-06 19:18:46 -0600

    add to config

--- server/config.py
+++ server/config.py
@@ -8,4 +8,25 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+app = Flask(__name__);
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.json.compact = False
 
+metadata = MetaData(naming_convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
+bcrypt = Bcrypt(app)
+app.secret_key = os.urandom(16)
+
+api = Api(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
