74365b1e825188ae545b5a3213b163e16495981f
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-06 23:09:33 -0600

    base route for app.py

--- server/app.py
+++ server/app.py
@@ -3,6 +3,14 @@
 from flask import request, session, abort
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+import datetime
+
 from config import app, db, api
 from models import User, Clothing, Blog, Custom
-import datetime
+
+@app.route('/')
+def index():
+    return '<h1>Milk n Peppers</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
