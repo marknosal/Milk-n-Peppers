8bea18fc222dbbd25495b88f18cf7a7abeb1df89
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-07 01:47:22 -0600

    complete User model

---
+++ server/models/user.py
@@ -0,0 +1,40 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from datetime import datetime, timedelta
+import ipdb
+
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-customs.user',)
+
+    # columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    # relationships
+    customs = db.relationship('Custom', back_populates='user')
+    clothings = association_proxy('customs', 'clothing')
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'<User: {self.id}. Username: {self.username}.>'
