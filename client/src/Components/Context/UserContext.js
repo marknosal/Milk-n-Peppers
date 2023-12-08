4a849e273adbddb402e477d102f7e5ac3305b19f
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-08 08:44:12 -0600

    add UserContext.js

---
+++ client/src/Components/Context/UserContext.js
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+import Login from "../Login/Login";
+
+const UserContext = createContext({})
+
+function UserProvider ({ children }) {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        fetch('/check_session').then(response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    login(data)
+                })
+            } else {
+                setUser(null)
+            }
+        })
+    }, [setUser])
+
+    function login(user) {
+        setUser(user)
+    }
+    function logout() {
+        setUser(null)
+    }
+
+    return (
+        <UserContext.Provider value = {{ user, setUser, login, logout }}>{children}</UserContext.Provider>
+    )
+}
