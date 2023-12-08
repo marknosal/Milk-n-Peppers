85b8b47d2d3ed1b1ce4f086b89beb733f2607598
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-08 04:53:06 -0600

    Login component

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 export default function Login () {
+    const [showLogin, setShowLogin] = useState(true)
     return (
-        <div>Login</div>
+        <div className="login">
+            {showLogin ? (
+                <div>
+                    <LoginForm />
+                    <hr/>
+                    Don't have an account?
+                    <button onClick={() => setShowLogin(!showLogin)}>
+                        Sign Up!
+                    </button>
+                </div>
+
+            ) : (
+                <div>
+                    <SignupForm />
+                    <hr/>
+                    Already have an account?
+                    <button onClick={() => setShowLogin(!showLogin)}>
+                        Log In!
+                    </button>
+                </div>
+            )}
+        </div>
     )
 }
