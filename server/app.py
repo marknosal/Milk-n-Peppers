515eafe0c008be66419208221d6295c843adeb77
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-08 06:55:19 -0600

    add /login route

--- server/app.py
+++ server/app.py
@@ -12,5 +12,36 @@ from models import User, Clothing, Blog, Custom
 def index():
     return '<h1>Milk n Peppers</h1>'
 
+@app.before_request
+def check_if_logged_in():
+    open_access_list = [
+        'signup',
+        'login',
+        'check_session',
+        'about',
+        'clothes',
+    ]
+    if request.endpoint not in open_access_list and not session.get('user_id'):
+        abort(401, 'Unauthorized')
+        
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        try:
+            user = User.query.filter_by(username=username).one_or_none()
+            if not user:
+                return {'error': f'username {username} not found'}, 404
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            elif not user.authenticate(password):
+                return {'error': 'username/password do not match'}, 401
+        except Exception as e:
+            return {'error': str(e)}, 401
+
+api.add_resource(Login, '/login', endpoint='login')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
