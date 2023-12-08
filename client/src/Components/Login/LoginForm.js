13025147ebcada67dc5ecd30adc6e73d907e5515
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-08 09:58:46 -0600

    apply UserContext to Login component

--- client/src/Components/Login/LoginForm.js
+++ client/src/Components/Login/LoginForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import Error from "../Error";
+import { UserContext } from "../Context/UserContext";
 
 export default function LoginForm () {
     const [error, setError] = useState(null)
+    const { login } = useContext(UserContext)
 
     const valSchema = yup.object().shape({
         username: yup.string().required('Must exist'),
