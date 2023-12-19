e31f0ed2ff64a72c0b2a8bc4406a7599b4ed88b9
Mark Nosal <mark.c.nosal@gmail.com>
2023-12-18 21:58:31 -0600

    implement cards for clothings

--- client/src/Components/Clothes/ClothingCard.js
+++ client/src/Components/Clothes/ClothingCard.js
@@ -3,7 +3,7 @@ import { Card } from "semantic-ui-react";
 export default function ClothingCard ({ clothing }) {
     return (
         <Card>
-            
+            <h3>{clothing.name}</h3>
         </Card>
     )
 }
