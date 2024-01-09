8485376b7c0adffbc1f4e894fd1a39ef796471a7
Mark Nosal <mark.c.nosal@gmail.com>
2024-01-09 03:23:42 -0600

    refactor Blog components

--- client/src/Components/Blog/Blog.jsx
+++ client/src/Components/Blog/Blog.jsx
@@ -1,48 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { Card, Header, Container } from "semantic-ui-react";
 import BlogCard from './BlogCard';
 import BlogExpand from "./BlogExpand";
-import '../../index.css'
+import '../../index.css';
 
-export default function Blog () {
-    const [blogs, setBlogs] = useState([])
-    const [expandedBlog, setExpandedBlog] = useState(null)
+export default function Blog() {
+    const [blogs, setBlogs] = useState([]);
+    const [expandedBlog, setExpandedBlog] = useState(null);
 
     useEffect(() => {
-        fetch('/blogs').then(response => {
-            response.json().then(data => {
-                setBlogs(data)
-            })
-        })
-    }, [])
+            fetch('/blogs').then(response => {
+                response.json().then(data => {
+                    setBlogs(data);
+                });
+            });
+    }, []);
 
     function handleExpand(id) {
         if (id) {
-            const blog = blogs.find(b => b.id === id)
-            setExpandedBlog(blog)
+            const blog = blogs.find(b => b.id === id);
+            setExpandedBlog(blog);
         } else {
-            setExpandedBlog(null)
+            setExpandedBlog(null);
         }
     }
 
     return (
-        <div className="blog">
-            <h1>Dayna's Blog:</h1>
+        <Container className="blog">
+            <Header as="h1" style={{ color: 'orange' }}>Dayna's Blog:</Header>
 
             {expandedBlog ? (
-                <BlogExpand blog={expandedBlog} onMinimize={handleExpand}  />
+                <BlogExpand blog={expandedBlog} onMinimize={handleExpand} />
             ) : (
-                <div className="blog-list">
-                    <ul>
-                        {blogs.map(blog => {
-                            return (
-                                <li>
-                                    <BlogCard blog={blog} onExpand={handleExpand} />
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
+                <Card.Group itemsPerRow={3}>
+                    {blogs.map(blog => (
+                        <BlogCard key={blog.id} blog={blog} onExpand={handleExpand} />
+                    ))}
+                </Card.Group>
             )}
-        </div>
-    )
+        </Container>
+    );
 }
--- client/src/Components/Blog/BlogCard.jsx
+++ client/src/Components/Blog/BlogCard.jsx
@@ -1,10 +1,16 @@
-export default function BlogCard ({ blog, onExpand }) {
-    function handleClick() {
-        onExpand(blog.id)
-    }
-    return (
-        <div onClick={handleClick}>
-            <h2>{blog.title}</h2>
-        </div>
-    )
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+export default function BlogCard({ blog, onExpand }) {
+  function handleClick() {
+    onExpand(blog.id);
+  }
+
+  return (
+    <Card color="orange" onClick={handleClick}>
+      <Card.Content>
+        <Card.Header>{blog.title}</Card.Header>
+      </Card.Content>
+    </Card>
+  );
 }
